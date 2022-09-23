ca33ff06f525e9be3c0535bdab5b733a0e7d625a
Manu <mcabral@frba.utn.edu.ar>
2022-09-23 17:48:20 -0300

    feat: add history query and supported browsers

--- pybrinf/constants.py
+++ pybrinf/constants.py
@@ -3,12 +3,14 @@ from pybrinf.exceptions import BrowserNotFound
 '''
 Contants values for PyBrinf.
 TODO: Add more browsers and linux / macos support.
+TODO: Modify the constants to be more dynamic for future browsers support.
 '''
 class Constants:
     '''Constants class for PyBrinf.'''
 
     DEFAULT_BROWSER_KEY = 'Software\\Microsoft\\Windows\\Shell\\Associations\\UrlAssociations\\http\\UserChoice'
     SUPPORTED_SYSTEMS = ['Windows']
+    SUPPORTED_BROWSERS = ['chrome', 'edge', 'yandex']
     SEARCH_PROCESS = 'WMIC PROCESS WHERE "name=\'{}\'" GET ExecutablePath'
 
     BROWSERS = [
@@ -48,23 +50,63 @@ class Constants:
         }
     ]
 
-    DOWNLOADS_QUERY = '''
+    HISTORY_QUERY = '''
+SELECT url, title, visit_count, last_visit_time
+FROM urls
+ORDER BY last_visit_time DESC
+'''
+
+    DOWNLOAD_QUERY = '''
 SELECT total_bytes, current_path, start_time, end_time, tab_url, url
 FROM downloads
 LEFT JOIN downloads_url_chains
 ON downloads.id = downloads_url_chains.id
+ORDER BY start_time DESC
 '''
+
+    @staticmethod
+    def set_filter(query: str, **kwargs) -> str:
+        '''
+        Set the filter for the query.
+
+        Args:
+            query (str): The query to filter.
+            **kwargs: The filter to apply.
+
+        Returns:
+            str: The filtered query.
+        '''
+        query += f' LIMIT {kwargs["limit"] if kwargs.get("limit") else 10}'
+        query += f' OFFSET {kwargs["offset"] if kwargs.get("offset") else 0}'
+        return query
+
+    @staticmethod
+    def history_query(**kwargs) -> str:
+        '''
+        Get the history query with the given parameters.
+
+        Args:
+            limit (int): The limit of the items to get. 
+            offset (int): The offset of the items to get.
+
+        Returns:
+            str: The history query.
+        '''
+        return Constants.set_filter(Constants.HISTORY_QUERY, **kwargs)
 
     @staticmethod
     def download_query(**kwargs) -> str:
         '''
         Get the query for downloads.
-        TODO: Implement limit, offset, and filters.
 
+        Args:
+            limit (int): The limit of the items to get.
+            offset (int): The offset of the items to get.
+        
         Returns:
             str: The query for downloads.
         '''
-        return Constants.DOWNLOADS_QUERY
+        return Constants.set_filter(Constants.DOWNLOAD_QUERY, **kwargs)
 
     @staticmethod
     def get_browser_data(name: str) -> dict:
