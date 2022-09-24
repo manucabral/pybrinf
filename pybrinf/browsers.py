0c96e863ef85bbd503ba2e5ad76acbc429ca532a
Manu <mcabral@frba.utn.edu.ar>
2022-09-24 19:03:07 -0300

    perf: separate browsers list to a one file

---
+++ pybrinf/browsers.py
@@ -0,0 +1,39 @@
+'''This file contains all browser necessary data.'''
+
+BROWSERS = [
+    {
+        'name': 'Chrome',
+        'fullname': 'Google Chrome',
+        'process': 'chrome.exe',
+        'progid': 'ChromeHTML',
+        'chromium': True,
+        'local_path': '\\Google\\Chrome\\User Data\\Default'
+    },
+    {
+        'name': 'Firefox',
+        'fullname': 'Mozilla Firefox',
+        'process': 'firefox.exe',
+        'progid': 'FirefoxHTML',
+        'chromium': False,
+        'app_path': 'ProgramFiles\\Mozilla Firefox\\firefox.exe',
+        'local_path': 'APPDATA\\Mozilla\\Firefox'
+    },
+    {
+        'name': 'Edge',
+        'fullname': 'Microsoft Edge',
+        'process': 'msedge.exe',
+        'progid': 'EdgeHTML',
+        'chromium': True,
+        'app_path': 'ProgramFiles(x86)\\Microsoft\\Edge\\Application\\msedge.exe',
+        'local_path': 'LOCALAPPDATA\\Microsoft\\Edge\\User Data\\Default'
+    },
+    {
+        'name': 'Yandex',
+        'fullname': 'Yandex Browser',
+        'process': 'browser.exe',
+        'chromium': True,
+        'progid': 'YandexHTML',
+        'app_path': 'LOCALAPPDATA\\Yandex\\YandexBrowser\\Application\\browser.exe',
+        'local_path': 'LOCALAPPDATA\\Yandex\\YandexBrowser\\User Data\\Default'
+    }
+]
