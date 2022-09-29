2a180333b8a6afb7c43f8a4cbda05d5f7bd3c357
Manu <mcabral@frba.utn.edu.ar>
2022-09-29 11:15:22 -0300

    style: lint

--- pybrinf/register.py
+++ pybrinf/register.py
@@ -1,40 +1,36 @@
-import winreg
-
 '''
 Register implementation for PyBrinf.
 This module uses winreg to read the registry.
 Docstrings are written in Google style.
 '''
 
+import winreg
+
 class Register:
     '''Register class core'''
 
-    def openkey(self, key: str, subkey: str, access: int = winreg.KEY_ALL_ACCESS) -> winreg.HKEYType:
+    def openkey(self, key: str, sub: str, access: int = winreg.KEY_ALL_ACCESS) -> winreg.HKEYType:
         '''
         Open a registry key.
 
         Args:
             key (str): The key to open.
-            subkey (str): The subkey to open.
+            sub (str): The subkey to open.
             access (int): The access to the key. Defaults to winreg.KEY_ALL_ACCESS.
-
         Raises:
             FileNotFoundError: The key does not exist.
             PermissionError: The access is denied.
             Exception: An unknown error occurred.
-        
         Returns:
             winreg.HKEYType: The opened key.
         '''
         try:
-            return winreg.OpenKey(key, subkey, 0, access)
-        except FileNotFoundError:
-            raise FileNotFoundError('The key or subkey does not exist.')
-        except PermissionError:
-            raise PermissionError('You do not have permission to access this key.')
-        except Exception as e:
-            raise e
-        
+            return winreg.OpenKey(key, sub, 0, access)
+        except FileNotFoundError as exc:
+            raise FileNotFoundError(f'Key {sub} not found') from exc
+        except PermissionError as exc:
+            raise PermissionError('Access denied') from exc
+
     def extract(self, key: winreg.HKEYType, value: str) -> str:
         '''
         Extract a value from a registry key.
@@ -42,17 +38,15 @@ class Register:
         Args:
             key (winreg.HKEYType): The key to extract the value from.
             value (str): The value to extract.
-        
         Raises:
             FileNotFoundError: The value does not exist.
             Exception: An unknown error occurred.
-        
         Returns:
             str: The extracted value.
         '''
         try:
             return winreg.QueryValueEx(key, value)[0]
-        except FileNotFoundError:
-            raise FileNotFoundError('The value does not exist.')
-        except Exception as e:
-            raise e
+        except FileNotFoundError as exc:
+            raise FileNotFoundError(f'Value {value} not found') from exc
+        except Exception as exc:
+            raise Exception(f'An unknown error occurred: {exc}') from exc
